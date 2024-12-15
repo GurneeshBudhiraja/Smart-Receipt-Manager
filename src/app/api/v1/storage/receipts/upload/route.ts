1fd47073fbc2e24e20ea9ea431c4331d39f59a5d
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-12-15 12:52:08 -0800

    Changes: -change the upload-image name to `receipts/upload` - create the helper functions for the `receipts/upload` route

---
+++ src/app/api/v1/storage/receipts/upload/route.ts
@@ -0,0 +1,133 @@
+import { NextRequest, NextResponse } from "next/server";
+import { uploadImage } from "@/utils/uploadImage";
+import { createAdminClient, createSessionClient } from "@/lib/appwrite/appwrite";
+import { createDoc, getDoc, updateDoc } from "@/lib/appwrite/document";
+export async function POST(request: NextRequest) {
+  const sessionClient = await createSessionClient();
+  let uploadedImageResponse;
+  try {
+    // Checks if the user is logged in
+    if (!sessionClient) {
+      return NextResponse.json(
+        {
+          error: 'Not authorized',
+          success: false
+        },
+        {
+          status: 401
+        }
+      )
+    }
+
+    const data = (await request.formData());
+
+    const imagePath = data.get('image');
+    const category = data.get('category');
+    const sideNotes = data.get('sidenotes');
+    const receiptText = data.get("receipttext");
+
+    const { account, storage, db } = sessionClient;
+
+    // TODO: remove this functionality from here and instead upload the image directly from the frontend to handle big image files
+    uploadedImageResponse = await uploadImage(storage, imagePath);
+
+    // if the image is not uploaded
+    if (!uploadedImageResponse) {
+      return NextResponse.json({
+        message: "Error uploading image",
+        success: false
+      }, { status: 500 })
+    }
+
+    const { $id: uploadedImageId } = uploadedImageResponse;
+    console.log("image id:", uploadedImageId);
+
+    // using userId as the documentId in the db
+    const { $id: documentId } = await account.get()
+    console.log(documentId);
+
+    const getDocResponse = await getDoc(db, documentId)
+
+    // if the document does not exist
+    if (getDocResponse === null) {
+      console.log("creating a new document");
+      const createDocResp =
+        await createDoc(
+          db, storage, documentId, uploadedImageId, process.env.NEXT_APPWRITE_IMAGE_COLLECTION_ID,
+          {
+            "image_ids": [`${category}_${uploadedImageId}`],
+          }
+        )
+      // not able to create a doc
+      if (createDocResp === null) {
+        return NextResponse.json(
+          {
+            message: "Error creating document",
+            success: false
+          }, { status: 500 }
+        )
+      }
+    } else {
+      // updates the existing document
+      const { image_ids } = getDocResponse;
+      console.log("image_ids:", image_ids);
+      const updatedDocResp = await updateDoc(db, storage, category, uploadedImageId, image_ids, documentId)
+      // not able to update the doc
+      if (updatedDocResp === null) {
+        return NextResponse.json(
+          {
+            message: "Error updating document",
+            success: false
+          }, { status: 500 }
+        )
+      }
+    }
+
+    // Document for storing the form data
+    const createDocResp = await
+      createDoc(
+        db, storage, uploadedImageId, uploadedImageId, process.env.NEXT_APPWRITE_NOTES_COLLECTION_ID,
+        {
+          category,
+          sidenotes: sideNotes,
+        }
+      )
+    if (createDocResp === null) {
+      return NextResponse.json(
+        {
+          message: "Error creating document",
+          success: false
+        }, { status: 500 }
+      )
+    }
+
+    return NextResponse.json({
+      message: "Data has been saved successfully",
+      success: true,
+    }, { status: 200 })
+
+  } catch (error) {
+    console.error(error);
+    console.log("DELETING IMAGES");
+
+    const { storage } = sessionClient;
+    if(uploadedImageResponse) {
+
+      const { $id: uploadedImageId } = uploadedImageResponse;
+      await storage.deleteFile(
+        process.env.NEXT_APPWRITE_BUCKET_ID,
+        uploadedImageId,
+      )
+      console.log("IMAGE DELETED FROM STORAGE");
+    }
+    return NextResponse.json(
+      {
+        message: "Error uploading image",
+        success: false
+      },
+      {
+        status: 500
+      }
+    )
+  }
+}
--- src/lib/appwrite/document.ts
+++ src/lib/appwrite/document.ts
@@ -1,6 +1,6 @@
-export async function getDoc(clientSessionDb, documentId) {
+export async function getDoc(db, documentId) {
   try {
-    const doc = await clientSessionDb.getDocument(
+    const doc = await db.getDocument(
       process.env.NEXT_APPWRITE_DB_ID,
       process.env.NEXT_APPWRITE_IMAGE_COLLECTION_ID,
       documentId
@@ -10,3 +10,49 @@ export async function getDoc(clientSessionDb, documentId) {
     return null
   }
 }
+
+export async function createDoc(db, storage, documentId, uploadedImageId, collectionId, props) {
+  try {
+    await db.createDocument(
+      process.env.NEXT_APPWRITE_DB_ID,
+      collectionId,
+      documentId,
+      props,
+    )
+  } catch (error) {
+    console.error(error);
+    console.log("DELETING THE IMAGE FROM STORAGE");
+    await storage.deleteFile(
+      process.env.NEXT_APPWRITE_BUCKET_ID,
+      uploadedImageId,
+    )
+    console.log("IMAGE DELETED FROM STORAGE");
+    return null
+  }
+}
+
+
+// TODO: fix the any types
+export async function updateDoc(db, storage, category, uploadedImageId, image_ids, documentId) {
+  try {
+    await db.updateDocument(
+      process.env.NEXT_APPWRITE_DB_ID,
+      process.env.NEXT_APPWRITE_IMAGE_COLLECTION_ID,
+      documentId,
+      {
+        "image_ids": [`${category}_${uploadedImageId}`, ...image_ids,]
+      }
+    )
+  } catch (error) {
+    console.error(error);
+    console.log("DELETING THE IMAGE FROM STORAGE");
+    await storage.deleteFile(
+      process.env.NEXT_APPWRITE_BUCKET_ID,
+      uploadedImageId,
+    )
+    console.log("IMAGE DELETED FROM STORAGE");
+    return null
+  }
+}
+
+
